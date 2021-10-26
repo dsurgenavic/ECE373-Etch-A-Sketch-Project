8987e6bf9ddd20adddaa86781bd04864683ab475
Darius Surgenavic <dsurgenavic@gmail.com>
2021-10-26 15:05:54 -0400

    added header file for storage system added main file

---
+++ Objects/storage.d
@@ -0,0 +1,2 @@
+./objects/storage.o: storage.c storage.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ main.c
@@ -0,0 +1,15 @@
+#include "stm32l053xx.h"
+#include "fsm_btn.h"
+#include "storage.h"
+
+void gpio_init(void);
+
+int main(void);
+
+void gpio_init(void) {
+	return;
+}
+
+int main(void) {
+	return 0;
+}
