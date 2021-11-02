ba77b9784fa80a1b9bbd3c970014765cc461839b
Darius Surgenavic <dsurgenavic@gmail.com>
2021-11-02 14:52:41 -0400

    added stuff to main.c

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
  *display_data
  */
 
-
+extern queue_t knob_queue;
+extern queue_t button_queue;
+extern queue_t display_data_semaphore;
 
 void gpio_init(void);
 int main(void);
@@ -19,5 +21,8 @@ void gpio_init(void) {
 }
 
 int main(void) {
+	init_queue(&knob_queue, 4);
+	init_queue(&button_queue, 2);
+	init_queue(&display_data_semaphore, 1);
 	return 0;
 }
--- storage.c
+++ storage.c
@@ -1,3 +1,17 @@
 #include "storage.h"
 
+void init_display_mem(void) {
+	for(uint8_t i = 0; i < 102; i++) {
+		for(uint8_t j = 0; j < 8; j++) {
+			display_mem[i][j] = 0x00;
+		}
+	}
+}
 
+void update_cursor_location() {
+	
+}
+
+void write_storage() {
+
+}
