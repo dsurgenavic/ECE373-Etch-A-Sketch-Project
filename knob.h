dbcac0847cd70336c1a5aade3db9386ce50952b1
Darius Surgenavic <dsurgenavic@gmail.com>
2021-11-11 15:25:06 -0500

    added function to run all task functions for storage update

--- knob.h
+++ knob.h
@@ -4,10 +4,6 @@
 #include "stdint.h"
 #include "fsm_btn.h"
 
-struct knob{
-	struct btn_struct *qa;
-	struct btn_struct *qb;
-};
 
 enum input_messages {UD_UP, UD_DOWN, LR_RIGHT, LR_LEFT, BTN_PRESS};
 
